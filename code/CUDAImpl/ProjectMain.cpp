bd328150396ff302d7a2589b8f083d13606c5c18
Esben Skaarup <sben@sben.dk>
2014-10-23 17:02:45 +0200

    Temporaruly removed timers. Compiles with nvcc now.

--- code/CUDAImpl/ProjectMain.cpp
+++ code/CUDAImpl/ProjectMain.cpp
@@ -1,4 +1,3 @@
-#include "OpenmpUtil.h"
 #include "ParseInput.h"
 
 #include "ProjHelperFun.h"
@@ -8,14 +7,14 @@
 #include <unistd.h>
 
 int main() {
-	timers_init();
+	//timers_init();
 	
 	unsigned int OUTER_LOOP_COUNT, NUM_X, NUM_Y, NUM_T; 
 	const REAL s0 = 0.03, strike = 0.03, t = 5.0, alpha = 0.2, nu = 0.6, beta = 0.5;
 	
 	readDataSet( OUTER_LOOP_COUNT, NUM_X, NUM_Y, NUM_T ); 
 	
-	const int Ps = get_CPU_num_threads();
+	const int Ps = 0;
 	REAL* res = (REAL*)malloc(OUTER_LOOP_COUNT*sizeof(REAL));
 	
 	{ // Original Program (Sequential CPU Execution)
--- code/CUDAImpl/timers.h
+++ code/CUDAImpl/timers.h
@@ -24,22 +24,35 @@ void timers_init();
 void timer_indent(int i);
 int timer_indent_get();
 
+#ifdef USE_TIMERS
 #define TIMER_DEFINE(name) \
 	struct timeval timer_##name##_total; \
 	struct timeval timer_##name##_accumulate; \
 	struct timeval timer_##name##_start; \
 	struct timeval timer_##name##_end; \
 	;
+#else
+#define TIMER_DEFINE(name)
+#endif
 
+#ifdef USE_TIMERS
 #define TIMER_INIT(name) \
 	timer_##name##_total.tv_usec = 0; \
 	timer_##name##_total.tv_sec = 0; \
 	;
+#else
+#define TIMER_INIT(name)
+#endif
 
+#ifdef USE_TIMERS
 #define TIMER_START(name) \
 	gettimeofday(&timer_##name##_start, NULL); \
 	;
+#else
+#define TIMER_START(name)
+#endif
 
+#ifdef USE_TIMERS
 #define TIMER_STOP(name) \
 	gettimeofday(&timer_##name##_end, NULL); \
 	timeval_subtract_( \
@@ -51,7 +64,12 @@ int timer_indent_get();
 			&timer_##name##_total, \
 			&timer_##name##_accumulate); \
 	;
+#else
+#define TIMER_STOP(name)
+#endif
 
+
+#ifdef USE_TIMERS
 #define TIMER_REPORT(name) { \
 	for(int i = 0; i < timer_indent_get(); i += 1) { \
 		printf("  "); \
@@ -61,6 +79,9 @@ int timer_indent_get();
 			14 - timer_indent_get() * 2, \
 			timeval_get_mu_s(&timer_##name##_total)); \
 };
+#else
+#define TIMER_REPORT(name)
+#endif
 
 //#define TIMER_GROUP(name_string) \
 //	for(int i = 0; i < timer_indent_get(); i += 1) { \
@@ -69,12 +90,21 @@ int timer_indent_get();
 //	printf("%s:\n", name_string); \
 //	timer_indent(1); \
 //	;
+#ifdef USE_TIMERS
 #define TIMER_GROUP() \
 	timer_indent(1); \
 	;
+#else
+#define TIMER_GROUP(name)
+#endif
 
+#ifdef USE_TIMERS
 #define TIMER_GROUP_END() \
 	timer_indent(-1); \
 	;
+#else
+#define TIMER_GROUP_END(name)
+#endif
+
 
 #endif // TIMERS_H
